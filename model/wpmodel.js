995d1f3796d7202dfdd0b9688a146305e5e46b5f
top80276 <top80276@yahoo.com.tw>
2022-07-10 21:17:48 +0800

    FIN9

--- model/wpmodel.js
+++ model/wpmodel.js
@@ -1,11 +1,52 @@
 var express = require('express');
-var mysql = require('mysql');
-var conn = mysql.createConnection({
-    host:'us-cdbr-east-06.cleardb.net',
+// var mysql = require('mysql');
+// var conn = mysql.createConnection({
+//     host:'us-cdbr-east-06.cleardb.net',
+//     user:'bec73f25ef8ac3',
+//     password:'b3fc3ad6',
+//     database:'heroku_9059c434d2fdb58'
+// });
+
+
+var mysql = require("mysql");
+var pool = mysql.createPool({
+    host: 'us-cdbr-east-06.cleardb.net',
     user:'bec73f25ef8ac3',
     password:'b3fc3ad6',
-    database:'heroku_9059c434d2fdb58'
+    database:'heroku_9059c434d2fdb58',
+    connectionLimit : 10, // 可以自己設定
 });
+
+var query = function(sql, options, callback) {
+    console.log(sql, options, callback);
+    if (typeof options === "function") {
+        callback = options;
+        options = undefined;
+    }
+    pool.getConnection(function(err, conn){
+        if (err) {
+            callback(err, null, null);
+        } else {
+            conn.query(sql, options, function(err, results, fields){
+                // callback
+                callback(err, results, fields);
+            });
+            // release connection。
+            // 要注意的是，connection 的釋放需要在此 release，而不能在 callback 中 release
+            conn.release();
+        }
+    });
+};
+
+module.exports = query;
+
+app.use(function (req, res, next) {
+    req.query = mysqlPoolQuery;
+    next();
+});
+
+
+
 
 
 //http://localhost:8181/weapon/queryall
